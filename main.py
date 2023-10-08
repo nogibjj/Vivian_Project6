662feefd1d12e9951d0493aeae4e38095e2e431b
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-08 16:11:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import sys
 import argparse
 from lib.query import test_query
+
+def handle_arguments(args):
+    """add action based on inital calls"""
+    parser = argparse.ArgumentParser(description="ETL-Query script")
+    parser.add_argument(
+        "action",
+        choices=[
+            "test_query"
+        ],
+    )
+    args = parser.parse_args(args[:1])
+    print(args.action)
+
+    if args.action == "test_query":
+        parser.add_argument("query")
+
+    # parse again with ever
+    return parser.parse_args(sys.argv[1:])
 
 def main():
     args = handle_arguments(sys.argv[1:])
