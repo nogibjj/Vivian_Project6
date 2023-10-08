91c9de684503d614b4e34672cc8c6b794244e101
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-08 16:54:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,14 @@ from lib.query import test_query
 
 def handle_arguments():
     parser = argparse.ArgumentParser(description="ETL-Query script")
-    parser.add_argument("action", choices=["test_query"])
+    parser.add_argument("query", help="Query to be tested")
 
-    args = parser.parse_args()
-
-    if args.action == "test_query":
-        parser.add_argument("query")
-        args = parser.parse_args()  # Parse again to include the 'query' argument
-
-    return args
+    return parser.parse_args()
 
 def main():
     args = handle_arguments()
-
-    if args.action == "test_query":
-        if hasattr(args, 'query'):
-            test_query(args.query)
-            return 0  # Indicate success
-        else:
-            print("Error: 'query' argument is required for 'test_query' action.")
-            return -1
-
-    # This block is technically not needed since argparse will handle invalid choices
-    # But it's kept here for clarity
-    else:
-        print(f"Unknown action: {args.action}")
-        return -1
+    test_query(args.query)
+    return 1  # Indicate success
 
 if __name__ == "__main__":
     sys.exit(main())
