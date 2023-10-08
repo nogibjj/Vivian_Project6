f0418d5a8b57f29032edba5f53910396f32fe065
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-08 16:25:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,34 @@ import sys
 import argparse
 from lib.query import test_query
 
-def handle_arguments(args):
-    """add action based on inital calls"""
+def handle_arguments():
     parser = argparse.ArgumentParser(description="ETL-Query script")
-    parser.add_argument(
-        "action",
-        choices=[
-            "test_query"
-        ],
-    )
-    args = parser.parse_args(args[:1])
+    parser.add_argument("action", choices=["test_query"])
+
+    args = parser.parse_args()
+
     if args.action == "test_query":
         parser.add_argument("query")
+        args = parser.parse_args()  # Parse again to include the 'query' argument
 
-    # parse again with ever
-    return parser.parse_args(sys.argv[1:])
+    return args
 
 def main():
-    args = handle_arguments(sys.argv[1:])
-    if args.action == "test_query":
-        test_query(args.query)
-        return 1
+    args = handle_arguments()
 
+    if args.action == "test_query":
+        if hasattr(args, 'query'):
+            test_query(args.query)
+            return 0  # Indicate success
+        else:
+            print("Error: 'query' argument is required for 'test_query' action.")
+            return -1
+
+    # This block is technically not needed since argparse will handle invalid choices
+    # But it's kept here for clarity
     else:
         print(f"Unknown action: {args.action}")
         return -1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
