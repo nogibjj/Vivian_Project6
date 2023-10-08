e3da1c4fab2274f6c7b054c95b97f86f4f76f202
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-08 17:43:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from lib.query import test_query
 
 def main():
+    parser = argparse.ArgumentParser(description="ETL-Query script")
     args = parser.parse_args(sys.argv[1:])
     test_query(args)
     return 1  # Indicate success
