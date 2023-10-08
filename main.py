e23a6495bf0a47aaee327044056c03991e9745b4
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-10-08 15:57:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import sys
 import argparse
 from lib.query import test_query
 
 def main():
+    args = handle_arguments(sys.argv[1:])
     if args.action == "test_query":
         test_query(args.query)
+        return 1
 
     else:
         print(f"Unknown action: {args.action}")
+        return -1
 
 if __name__ == "__main__":
     main()
